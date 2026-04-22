72c0b52db7470bb49614b5bc0070f7f6e767289a
lefv <luis@lefv.io>
2026-04-22 11:08:16 -0400

    test: register Mox BrokerMock + broker config map

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,13 @@
 ExUnit.start()
 
 {:ok, _} = AlpacaTrader.Brokers.Mock.start_link()
+
+# Mox defmock for the Broker behaviour. Used by OrderRouter tests
+# that need fine-grained expectations (verify_on_exit!).
+Mox.defmock(AlpacaTrader.BrokerMock, for: AlpacaTrader.Broker)
+
+Application.put_env(:alpaca_trader, :brokers,
+  alpaca: AlpacaTrader.Brokers.Alpaca,
+  mock: AlpacaTrader.Brokers.Mock,
+  broker_mock: AlpacaTrader.BrokerMock
+)
